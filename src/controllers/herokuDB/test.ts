3dae0256f2411216afd407bb9f0ea4f071f00f9d
JordanSerafini <jordan@solution-logique.fr>
2024-03-08 16:37:17 +0100

    workinf

--- src/controllers/herokuDB/test.ts
+++ src/controllers/herokuDB/test.ts
@@ -14,6 +14,19 @@ const test = {
           res.status(500).send("Erreur lors de la récupération des données.");
         }
     },
+
+    async getAllAdress(req: any, res: any) {
+        try {
+            const query = "SELECT * FROM \"Address\";";
+            const tables = await client.query(query);
+  
+          res.send(tables);
+  
+        } catch (err) {
+          console.log(err);
+          res.status(500).send("Erreur lors de la récupération des données.");
+        }
+    },
 };
 
 export default test;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,11 +3,17 @@ import express from "express";
 import test from "../controllers/herokuDB/test";
 
 const router = express.Router();
+
+
 router.get('/', (req, res) => {
     res.send('Hello, World!');
   });
 
 
+  // Route pour les Items:
 router.get('/getAllitem', test.getAllitem);
+
+// Route pour les Adresses:
+router.get('/getAllAdress', test.getAllAdress);
 
   export default router
