6a08da6b95f77efcaff5a5337f5b09aa8cf89b18
JordanSerafini <jordan@solution-logique.fr>
2024-03-08 16:28:23 +0100

    workinf

--- src/database/client-pool/herokuBDD.ts
+++ src/database/client-pool/herokuBDD.ts
@@ -1,24 +1,24 @@
-const { Client } = require('pg');
+import { Client } from 'pg';
 
 const client = new Client({
-  user: 'uctk97771r0prk',
-  host: 'cfua00420e2gff.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
-  database: 'd5dqpkj8k7ts86',
-  password: 'p490985d3e78c3c4866337560ae2beff8aada5ce48617b8e1f54c5f697193e406',
-  port: '5432',
-  ssl: {
-    rejectUnauthorized: false 
-  }
+    user: 'uctk97771r0prk',
+    host: 'cfua00420e2gff.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
+    database: 'd5dqpkj8k7ts86',
+    password: 'p490985d3e78c3c4866337560ae2beff8aada5ce48617b8e1f54c5f697193e406',
+    port: 5432, 
+    ssl: {
+        rejectUnauthorized: false 
+    }
 });
 
 client.connect();
 
-client.query('SELECT NOW()', (err: any, res: { rows: any; }) => {
+client.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.error(err);
   } else {
     console.log(res.rows);
-    client.end();
+   
   }
 });
 
